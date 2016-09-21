32e6aae76f63ef78dfa973ac1fd7a47dc28d62d3
Niklas <niklas.frank@gmail.com>
2016-09-21 08:00:19 +0200

    added conversationthread to init state

--- client/__src/state.jsx
+++ client/__src/state.jsx
@@ -1,14 +1,15 @@
-import Observable from 'rxjs/Observable';
+import Rs from 'rxjs';
 import createState from 'app/rx-state/createState';
 import ChatReducer$ from 'app/reducers/ChatReducer';
 import http from 'app/service/http';
 
-const reducer$ = Rx.Observable.merge(
+const reducer$ = Rs.Observable.merge(
   ChatReducer$
 );
 
-const initialState$ = Observable.of({
-  conversations: []
+const initialState$ = Rs.Observable.of({
+  conversations: [],
+  conversationThread: []
 });
 
 export default createState(reducer$, initialState$);
