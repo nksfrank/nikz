e386f4e5a484ebafc0ed95a40c1123db908433fd
Niklas <niklas.frank@gmail.com>
2016-09-21 08:02:33 +0200

    adding response to state

--- client/__src/reducers/chatReducer.jsx
+++ client/__src/reducers/chatReducer.jsx
@@ -3,8 +3,6 @@ import ChatActions from '../actions/chat-actions';
 
 const ChatReducer$ = Observable.merge(
   ChatActions.fetchConversation$
-    .flatMap(conversationId => http.get(conversationId))
-    .map(data => {
-
-    })
+    .flatMap(conversationId => http.get(`/messages/${conversationId}`))
+    .map(data => state => ({...state, conversationThread: data}))
 );
