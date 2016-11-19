4b34a47a94c69be9b1a5ddeb7b1d3d1acf6f5b88
Niklas <niklas.frank@gmail.com>
2016-11-19 10:00:37 +0100

    Fix send

--- client/__src/components/chat/message-inputbox.jsx
+++ client/__src/components/chat/message-inputbox.jsx
@@ -7,44 +7,26 @@ require('./message-inputbox.scss');
 require('../../styles/segments.scss');
 
 class MessageBox extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {val:''}
-	}
 	componentDidMount() {
     this.setInputFocus();
 	}
-
-	handleSendClick = (e) => ChatActions.send$.next({conversation: this.props.conversation, e});
-	// 	if(this.state.message == undefined || this.state.message === '') {
-	// 		return
-	// 	}
-	// 	let txt = this.state.message;
-	// 	 TODO: send message to backend 
-
-	// 	this.setState({message: ''});
-	// 	this.setState({dirty: false});
-	// 	this.setInputFocus();
-	// 	this.props.onSendClick(txt)
-	// }
 
 	setInputFocus() {
 		this._input.focus();
 	}
 
-	handleBlur = () => this.setState({dirty: true});
-	handleChange = (e) => ChatActions.keypress$.next({conversation: this.props.conversation, e});
+	handleSend = (e) => ChatActions.send$.next({conversation: this.props.conversation, e});
+	handleChange = (e) => {ChatActions.keypress$.next({conversation: this.props.conversation, e})};
 	changeOnEnter = () => ChatActions.enterToSend$.next();
 
 	render() {
 		let {conversation} = this.props;
-		let btnSend = (<div className="messageSendButton"><button disabled={conversation.enterToSend} onClick={this.handleSendClick} className={`${conversation.enterToSend ? 'disabled': ''} button confirm`}>Send</button></div>);
-
+		let btnSend = (<div className="messageSendButton"><button disabled={conversation.enterToSend} onClick={this.handleSend} className={`${conversation.enterToSend ? 'disabled': ''} button confirm`}>Send</button></div>);
 		return (
-			<div className="message-input container" onBlur={this.handleBlur}>
+			<div className="message-input container">
 				<div className="segments">
 					<div className="segment inputbox">
-						<textarea autoFocus rows="3" value={conversation.inputValue} ref={(c) => this._input = c} onChange={this.handlechange} onKeyPress={this.handlechange}></textarea>
+						<textarea autoFocus rows="3" value={conversation.inputValue} ref={(c) => this._input = c} onChange={this.handleChange} onKeyPress={this.handleSend}></textarea>
 					</div>
 					<div className="segment toolbar">
 						<div></div>
--- client/__src/reducers/chatReducer.jsx
+++ client/__src/reducers/chatReducer.jsx
@@ -1,8 +1,10 @@
 import {Observable} from "rxjs/Observable";
 import ChatActions from "../actions/chat-actions";
 
+const postSettings = conversation => ({url: `http://localhost:3090/messages/${conversation.id}`, method: 'POST', body: {'message':conversation.inputValue}, headers: {'Content-Type':'application/json'}})
 const retry = obs => obs.zip(Observable.range(1,3), (_, i) => i).flatMap(i => Observable.timer(i * 1000));
-const initialState = {id: 1, enterToSend: false, messages: [{"id":"d1d3630e-9d3d-4421-8fb2-f73ef861d2f1","timeStamp":1478905399256,"message":"lorem ipsum"}]};
+
+const initialState = {id: 1, inputValue: '', enterToSend: false, messages: []};
 const ChatReducer$ = Observable.of(() => initialState)
   .merge(
     ChatActions.fetch$.switchMap(threadId => 
@@ -13,24 +15,26 @@ const ChatReducer$ = Observable.of(() => initialState)
     ).map(payload => state => Object.assign({}, state, payload)),
 
     ChatActions.send$
-      .filter(({conversation, e}) => {console.log(conversation, e); return conversation.inputValue && conversation.inputValue.length > 0})
-      .switchMap(({conversation, e}) => Observable.ajax({url: `http://localhost:3090/messages/${conversation.id}`, method: 'POST', body: {'message':conversation.inputValue}, headers: {'Content-Type':'application/json'}})
+      //press on send button
+      .filter(({conversation, e}) => e.type === 'click' && conversation.inputValue && conversation.inputValue.length > 0)
+      .switchMap(({conversation, e}) => Observable.ajax(postSettings(conversation))
         .map(response => response.response)
         .catch(Observable.empty()))
       .map(message => state => Object.assign({}, state, {inputValue: '', messages: [...state.messages, message]})),
 
-    ChatActions.keypress$
-      .filter(({conversation, e}) => e.target.value && !(conversation.enterToSend && e.key === 'Enter' && !e.shiftKey) || e.key !== 'Enter')
-      .map(({conversation, e}) => {console.log(e, e.target, e.target.value); return state => Object.assign({}, state, {inputValue: e.target.value})}),
+    ChatActions.send$
+      //hitting enter to send
+      .filter(({conversation, e}) => e.type === 'keypress' && conversation.enterToSend && !e.shiftKey && e.key === 'Enter' && conversation.inputValue && conversation.inputValue.length > 0)
+      .switchMap(({conversation, e}) => Observable.ajax(postSettings(conversation))
+          .map(response => response.response)
+          .catch(Observable.empty()))
+      .map(message => state => Object.assign({}, state, {inputValue: '', messages: [...state.messages, message]})),
 
     ChatActions.keypress$
-    .filter(({conversation, e}) => e && conversation.enterToSend && !e.shiftKey && e.key === 'Enter')
-    .switchMap(({conversation, e}) => Observable.ajax({url: `http://localhost:3090/messages/${conversation.id}`, method: 'POST', body: {'message':conversation.inputValue}, headers: {'Content-Type':'application/json'}})
-        .map(response => response.response)
-        .catch(Observable.empty()))
-      .map(message => {console.log("send"); return state => Object.assign({}, state, {inputValue: '', messages: [...state.messages, message]})}),
+      .filter(({conversation, e}) => e.type === 'change' && !(conversation.enterToSend && e.shiftKey && e.key === 'Enter'))
+      .map(({conversation, e}) => state => Object.assign({}, state, {inputValue: e.target.value})),
     
-    ChatActions.enterToSend$.map(_payload => state => Object.assign({}, state, {enterToSend:!state.enterToSend}))
+    ChatActions.enterToSend$.map(_payload => state => Object.assign({}, state, {enterToSend:!state.enterToSend})) 
   );
 
 export default ChatReducer$
