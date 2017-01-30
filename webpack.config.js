b04839f2c7936774451a84a6e73abba8c71c540e
Niklas Frank <niklas.frank@gmail.com>
2017-01-30 21:05:43 +0100

    Setup working config

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,57 @@
 var webpack = require('webpack'),
-	ExtractTextPlugin = require('extract-text-webpack-plugin'),
-	path = require('path');
+  ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  path = require('path');
 
 function getEntrySources(sources) {
-	if(process.env.NODE_ENV !== 'production') {
-		sources.push('webpack-dev-server/client?http://localhost:8080');
-		sources.push('webpack/hot/only-dev-server');
-	}
-	return sources;
+  if(process.env.NODE_ENV !== 'production') {
+    sources.push('webpack-dev-server/client?http://localhost:8080');
+    sources.push('webpack/hot/only-dev-server');
+  }
+  return sources;
 }
 module.exports = {
-	entry: './client/__src/index.jsx',
+  entry: {
+    app: [
+      './src/views/root/index.tsx',
+    ]
+  },
   output: {
-    path: path.resolve(__dirname, 'client/'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'www/dist/'),
+    publicPath: '/dist/',
+    filename: '[name].bundle.js',
     sourceMapFilename: '[file].map'
   },
-	resolve: {
-		root: path.resolve(__dirname),
-		alias: {
-			app: 'client/__src',
-			service: 'client/__src/service',
-			util: 'client/__src/util'
-		},
-		extensions: ['', '.js', '.jsx']
-	},
-	module:{
-		loaders: [{
-			test: /\.jsx?$/,
-			loader: 'babel',
-			query: {
-				presets: [
-					'es2015',
-					'react',
-					'stage-0'
-				]
-			},
-			exclude: /(node_modules|bower_components)/
-		},
-		{
-			test: /\.scss$/,
-			loader: ExtractTextPlugin.extract('style-loader', 'css?sourceMap!sass?sourceMap'),
-			exclude: /(node_modules|bower_components)/
-		}
-	]},
-	plugins: [
-		new ExtractTextPlugin('style.css', {
-			allChunks: true
-		})
-	],
-	sassLoader: {
-    includePaths: [ 'client/__src/styles' ]
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.scss', '.css']
   },
+  module:{
+    rules: [{
+      test: /\.tsx?$/,
+      loader: 'awesome-typescript-loader',
+      exclude: /(node_modules|bower_components)/
+    },
+    {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract({
+        fallbackLoader: 'style-loader',
+        loader: 'css?sourceMap!sass?sourceMap'
+      }),
+      exclude: /(node_modules|bower_components)/
+    }]
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },
+  plugins: [
+    new ExtractTextPlugin({filename: 'style.css',
+      disable: false,
+      allChunks: true
+    })
+  ],
   devtool: 'eval-source-map',
-	devServer: {
-		historyApiFallback: true,
-		contentBase: './client/'
-	}
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './www/'
+  }
 };
