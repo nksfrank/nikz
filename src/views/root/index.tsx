cf434cd339df8bd87eaf93685afe5a2b2b4dd313
Niklas Frank <niklas.frank@gmail.com>
2017-01-30 21:06:40 +0100

    Add entry view

---
+++ src/views/root/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import Page from '../../components/page/';
+
+ReactDOM.render(
+  <Page text="Hello World" />,
+  document.getElementById("root")
+);
