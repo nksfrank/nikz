2dd02a0f1d9bb68429b594454cf118111ca34de7
niklas.frank <niklas.frank@gmail.com>
2017-02-05 13:21:05 +0100

    Move entry to root folder

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
+import connect from 'platform/connect';
 
 export interface HelloProps {
   text: string;
 }
 
-export default class Page extends React.Component<HelloProps, undefined> {
+class Page extends React.Component<HelloProps, void> {
   render() {
     return(
       <div>{this.props.text}</div>
     );
   }
 }
+
+export default connect(
+  state => {
+
+  },
+  dispatch => {
+
+  }
+)(Page) as React.ComponentClass<HelloProps>;
