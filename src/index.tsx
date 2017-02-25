29f7d625249912e1cb3fd96c1790cc6cc1990676
niklas.frank <niklas.frank@gmail.com>
2017-02-25 09:27:37 +0100

    setup home route

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-import { createStore } from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reducers from './components/shared/platform/reducers';
 
 import Page from './components/page';
 
-const store = createStore(reducers);
-const history = syncHistoryWithStore(browserHistory, store)
+let store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
+    <Router>
       <Route path="/" component={Page}>
       </Route>
     </Router>
