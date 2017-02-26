01173ca31009d46dc3406deae499ef79252a2ce0
niklas.frank <niklas.frank@gmail.com>
2017-02-26 13:27:35 +0100

    add segment container

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import * as styles from './base.scss';
 import Card, {Section, Title, Image} from '../shared/Card';
+import {Segment} from '../shared/Layout';
 import connect from 'platform/connect';
 
 export default () =>
-  <div className={styles.slider}>
+  <Segment apperance={['grouped', 'horizontal']}>
     <Card title="Welcome" apperance={['invert', 'overlay']} description="Selection" img={{src:'https://unsplash.it/720/480/?random=4'}} />
     <Card type="full">
       <Image src='https://unsplash.it/720/480/?random=2'>
@@ -28,4 +29,4 @@ export default () =>
         Welcome to the Chill
       </Title>
     </Card>
-  </div>;
+  </Segment>;
